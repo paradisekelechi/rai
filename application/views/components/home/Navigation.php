d8092e87537a226ddeb887bb2837c0a075104e3b
Paradise Kelechi <paradisekelechi@gmail.com>
2018-06-14 13:11:51 +0100

    fix the landing page responsiveness

---
+++ application/views/components/home/Navigation.php
@@ -0,0 +1,62 @@
+<nav>
+    <div class="contact-links-wrapper">
+        <ul class="contact-links">
+            <li>
+                <a href="">
+                    <i class="fas fa-phone" style="transform: rotateY(180deg);transform-origin:  center center;"></i>
+                    <span>(610)-183-4563</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="far fa-envelope"></i>
+                    <span>mail@motorius.com</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="social-links-wrapper">
+        <ul class="social-links">
+            <li>
+                <a href="">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fab fa-twitter"></i>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fab fa-google-plus-g"></i>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fab fa-linkedin-in"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="site-nav__left">
+        <div class="logo">
+            <img src="<?php echo base_url();?>assets/images/rai_logo.png">
+        </div>
+
+    </div>
+    <div class="site-nav__right">
+        <ul class="nav-list">
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Listing</a>
+            </li>
+            <li>
+                <a href="#">Services</a>
+            </li>
+        </ul>
+    </div>
+</nav>
